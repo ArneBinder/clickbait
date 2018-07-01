732e263857304167317f29428f8bdb92888b5bf9
Arne Binder <arne.b.binder@gmail.com>
2018-07-01 17:26:25 +0200

    fixed model and script

--- predict.sh
+++ predict.sh
@@ -6,5 +6,4 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 #echo "use model from $MODEL_DIR"
 
 cd "$SCRIPT_DIR"
-python spacy_deep_learning_keras.py --model-dir model_noimages_best "$@"
-
+python spacy_deep_learning_keras.py --model-dir model_wimages_best "$@"
